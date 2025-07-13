d28c4430e012f1eb46490eebbd2ce4354b2866c4
Edmund Tekpenor <slicegamen@gmail.com>
2025-07-13 17:44:10 +0000

    add multi languages loops file

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+const lines = [
+    "C is fun" ,
+    "Python is cool" ,
+    "JavaScript is amazing"
+];
+
+let outputString = "";
+
+for (const line of lines) {
+    outputString += line + "\n";
+}
+console.log(outputString);
